c1d033fdc729491e49a2c36ee679972eb3c795a6
Proewyn <brice.losson7@etu.univ-lorraine.fr>
2019-04-15 14:20:19 +0200

    Debut de fonction pour lire un message, à finir

--- src/filtre/Filtre_mail.java
+++ src/filtre/Filtre_mail.java
@@ -24,10 +24,44 @@ public class Filtre_mail {
 	 */
 	protected void charger_dictionnaire(String chemin) {
 		try (Stream<String> stream = Files.lines(Paths.get(chemin)).filter(s -> s.length() > 3)) {
-			stream.forEach(e -> dico.add(e));
+			stream.forEach(e -> dico.add(e.toUpperCase()));
 		}
 		catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Parcourt tout les mots d'un mail et verifie leur presence ou non par rapport au dictionnaire
+	 * @param message Nom du fichier contenant le message
+	 * @return Une liste de boolean indiquant la presence ou non d'un mot du dictionnaire
+	 */
+	protected ArrayList<Boolean> lire_message(String message) {
+		ArrayList<Boolean> representation = new ArrayList<>(getDico().size());
+		try (Stream<String> stream = Files.lines(Paths.get(message))) {
+			stream.forEach(e -> verifieMot(e.toUpperCase()));
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return representation;
+	}
+
+	/**
+	 * Casse la ligne en plusieurs mot et verifie leur presence ou non par rapport au dictionnaire
+	 * @param line Ligne de mot
+	 */
+	private void verifieMot(String line) {
+		line = line.toUpperCase();
+		String[] splitted = line.split(" ");
+		ArrayList<String> dico = getDico();
+		for (String mot : splitted) {
+			for (int i = 0 ; i < dico.size() ; i++) {
+				if (dico.get(i) == mot) {
+					//Passe le boolean de presence à l'index i à True/1
+				}
+			}
 		}
 	}
 	
